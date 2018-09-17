0987ade13f0b0a4e32689ec781c393a9d049e783
Shasha <40842477+Shasha0425@users.noreply.github.com>
2018-09-17 00:41:30 -0400

    Styling

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+// console.log("hello")
+   
+// alert("Hello World");
+   
+   
+   
+   // Here we created an on-click event that responds to button clicks of the crystal image.
+   
+    // $(".gem").on("click", function() {
+  
+    //     // Clicking the button triggers an alert message.
+    //     alert("You clicked a crystal!");
+    
+    //   });
